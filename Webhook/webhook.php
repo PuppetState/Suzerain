74f7ddf8931423d98d13bc0c5892bc258abad3c5
PuppetState <100985757+PuppetState@users.noreply.github.com>
2022-03-16 21:54:47 +0800

    Create webhook.php
    upload webhook to auto pull

---
+++ Webhook/webhook.php
@@ -0,0 +1,13 @@
+<?php
+ $where = __DIR__
+ $serverdo = 'cd' $where '|git pull'
+ $serverdid = shell_exec($serverdo);
+print $serverdid;
+
+/***
+ * A webhook for auto clone $ pull
+ * Not full code, need another code to make it run.
+ * It's not running version at server now.
+ */
+
+?>
